017959f3f2f3d0b6f8e8e35eb5c4a447fa3e567c
specific008 <ningpengtao1995@gmail.com>
2016-11-02 13:01:25 +0800

    add User model and fix some config

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,34 @@
 #!flask/bin/python
 # encoding: utf-8
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_wtf.csrf import CsrfProtect
+from flask_moment import Moment
+from config import Config
+from flask_mail import Mail
+
+db = SQLAlchemy()
+moment = Moment()
+bootstrap = Bootstrap()
+mail = Mail()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = ''
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    Config.init_app(app)
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
+
+    return app
--- config.py
+++ config.py
@@ -18,6 +18,19 @@ class Config:
     WTF_CSRF_ENABLED = True
     SECRET_KEY = 'uestc2014'
 
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+
+    POSTS_PER_PAGE = 10
+
     @staticmethod
-    def init_app(self, app):
+    def init_app(app):
         pass
