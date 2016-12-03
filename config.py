9a3993b9ea4308b1b53292b4fd470c9adb4ac2f3
liupengkd <1374294826@qq.com>
2016-12-03 16:11:18 +0800

    login and register

--- config.py
+++ config.py
@@ -2,9 +2,7 @@
 # encoding: utf-8
 
 import os
-
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config:
     def __init__(self):
@@ -18,6 +16,50 @@ class Config:
     WTF_CSRF_ENABLED = True
     SECRET_KEY = 'uestc2014'
 
+    SSL_DISABLE = False
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_RECORD_QUERIES = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    MAIL_SERVER = 'smtp.163.com'  # 'smtp.googlemail.com'
+    MAIL_PORT = '25'  # 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = '15680781730@163.com'  # os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = 'liupeng527527'  # os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = '15680781730@163.com'  # 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    # FLASKY_POSTS_PER_PAGE = 20
+    # FLASKY_FOLLOWERS_PER_PAGE = 50
+    # FLASKY_COMMENTS_PER_PAGE = 30
+    # FLASKY_SLOW_DB_QUERY_TIME = 0.5
+
+    POSTS_PER_PAGE = 10
+
     @staticmethod
-    def init_app(self, app):
+    def init_app(app):
         pass
+
+    # @classmethod
+    # def init_app(cls, app):
+    #     Config.init_app(app)
+    #
+    #     # email errors to the administrators
+    #     import logging
+    #     from logging.handlers import SMTPHandler
+    #     credentials = None
+    #     secure = None
+    #     if getattr(cls, 'MAIL_USERNAME', None) is not None:
+    #         credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+    #         if getattr(cls, 'MAIL_USE_TLS', None):
+    #             secure = ()
+    #     mail_handler = SMTPHandler(
+    #         mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+    #         fromaddr=cls.FLASKY_MAIL_SENDER,
+    #         toaddrs=[cls.FLASKY_ADMIN],
+    #         subject=cls.FLASKY_MAIL_SUBJECT_PREFIX + ' Application Error',
+    #         credentials=credentials,
+    #         secure=secure)
+    #     mail_handler.setLevel(logging.ERROR)
+    #     app.logger.addHandler(mail_handler)
+
