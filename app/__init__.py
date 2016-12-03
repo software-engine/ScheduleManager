d94071263d089e1b3a0583ada8cf1c2318f09c2b
liupengkd <1374294826@qq.com>
2016-12-03 15:57:46 +0800

    login and register

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,41 @@
 #!flask/bin/python
 # encoding: utf-8
+
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_wtf.csrf import CsrfProtect
+from flask_moment import Moment
+from config import Config
+from flask_mail import Mail
+
+db = SQLAlchemy()
+moment = Moment()
+bootstrap = Bootstrap()
+mail = Mail()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'login.user_login'
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    Config.init_app(app)
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    mail.init_app(app)
+    login_manager.init_app(app)
+
+    from .login import login as login_blueprint
+    app.register_blueprint(login_blueprint)
+
+    from .register import register as register_blueprint
+    app.register_blueprint(register_blueprint, url_prefix='/register')
+
+    return app
--- app/models.py
+++ app/models.py
@@ -1,2 +1,118 @@
 #!flask/bin/python
 # encoding: utf-8
+
+import hashlib
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from . import db, login_manager
+from flask import current_app, request
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    email = db.Column(db.String(64), unique=True)
+    identity = db.Column(db.String(5))
+    password_hash = db.Column(db.String(128))
+    avatar_hash = db.Column(db.String(32))
+    confirmed = db.Column(db.Boolean, default=False)
+
+    @property
+    def password(self):
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+
+    def gravatar(self, size=40, default='identicon', rating='g'):
+        if request.is_secure:
+            url = 'https://secure.gravatar.com/avatar'
+        else:
+            url = 'http://www.gravatar.com/avatar'
+        hash = self.avatar_hash or hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        return '{url}/{hash}?s={size}&d={default}&r={rating}'.format(
+            url=url, hash=hash, size=size, default=default, rating=rating)
+
+    def generate_confirmation_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'confirm': self.id})
+
+    def confirm(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('confirm') != self.id:
+            return False
+        self.confirmed = True
+        db.session.add(self)
+        db.session.commit()
+        return True
+
+    def generate_reset_password_token(self, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'password': self.id})
+
+    def reset_password(self, token, new_password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        if data.get('password') != self.id:
+            return False
+        self.password = new_password
+        db.session.add(self)
+        db.session.commit()
+        return True
+
+    def generate_change_email_token(self, new_email, expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'], expiration)
+        return s.dumps({'email': self.id, 'new_email': new_email})
+
+    def change_email(self, token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return False
+        new_email = data.get('new_email')
+        if new_email is None:
+            return False
+        if self.query.filter_by(email=new_email).first() is not None:
+            return False
+        if data.get('email') != self.id:
+            return False
+        self.email = new_email
+        self.avatar_hash = hashlib.md5(self.email.encode('utf-8')).hexdigest()
+        db.session.add(self)
+        db.session.commit()
+        return True
+
+    def change_name(self, new_name):
+        if self.query.filter_by(name=new_name).first() is not None:
+            return False
+        self.name = new_name
+        db.session.add(self)
+        db.session.commit()
+        return True
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
